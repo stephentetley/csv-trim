d42d2a3e4fda2b7ebc9a8d6751c9941ccf3ce3d0
stephentetley <stephen.tetley@gmail.com>
2018-10-25 14:29:37 +0100

    Adding a prototype of records, which is exploring the way towards dynamic rows (i.e. adding, removing and mapping over cells).

---
+++ test/CsvProvider01.fsx
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+open System.IO
+
+// Use FSharp.Data for CSV reading
+#I @"..\packages\FSharp.Data.3.0.0-beta3\lib\net45"
+#r @"FSharp.Data.dll"
+open FSharp.Data
+
+
+type CsvProTable = 
+    CsvProvider< Sample = "A,B,C\n1,2,3\n1,3,2\n",
+                 Schema = "A(int),B(int),C(int)",
+                 HasHeaders = true >
+
+type CsvProRow = CsvProTable.Row
+
+let test01 () = 
+    (new CsvProTable()).SaveToString()
+
---
+++ test/TempRecords.fsx
@@ -0,0 +1,63 @@
+﻿
+// Records are heterogenous assoc lists
+type Record = (string * obj) list
+
+let empty : Record = []
+
+let select (label:string) (source:Record) : obj = 
+    let rec work xs = 
+        match xs with
+        | [] -> failwithf "Missing field: %s" label
+        | (s,o) :: rest ->
+            if s = label then o else work rest
+    work source
+
+let extend (label:string) (value:obj) (source:Record) : Record = 
+    (label,value) :: source
+
+
+    
+let restrict (label:string) (source:Record) : Record = 
+    let rec cat xs ys = 
+        match xs with
+        | [] -> ys
+        | z::zs -> cat zs (z::ys)
+    let rec work ac xs = 
+        match xs with
+        | (s,o) :: rest ->
+            if s = label then 
+                cat ac rest 
+            else
+                work ((s,o) :: ac) rest
+        | [] -> source
+    work [] source
+
+let update (label:string) (value:obj) (source:Record) : Record = 
+    source |> restrict label |> extend label value
+
+let rename (oldName:string) (newName:string) (source:Record) : Record = 
+    let o = select oldName source
+    source |> restrict oldName |> extend newName o
+
+let labels (source:Record) : string list = 
+    List.map fst source
+
+let concatL (r1:Record) (r2:Record) : Record = 
+    List.fold (fun ac (s,o) -> extend s o ac) r2 r1
+
+let concatR (r1:Record) (r2:Record) : Record = 
+    List.fold (fun ac (s,o) -> extend s o ac) r1 r2
+
+
+
+
+// dummy
+let r1 = empty |> extend "Name" ("stephen" :> obj) |> extend "Age" (46 :> obj)
+
+let r2 = r1 |> restrict "Name"
+
+let r3 = r1 |> update "Age" (47 :> obj)
+
+let r4 = r1 |> rename "Name" "First Name"
+
+let ls = labels r4
