3131d89ceb9ac0c641b95a80378ec536284f9c82
stephentetley <stephen.tetley@gmail.com>
2018-10-25 11:59:52 +0100

    More work on OO style output (to replace the monadic output).

--- src/DynaCsv/CsvOutput2.fs
+++ src/DynaCsv/CsvOutput2.fs
@@ -1,13 +1,26 @@
 ﻿// Copyright (c) Stephen Tetley 2018
 // License: BSD 3 Clause
 
-module CsvTrim.CsvOutput2
+module DynaCsv.CsvOutput2
 
 open System
 open System.IO
 
 
-let quoteValue (quote:char) (input:string) : string = 
+/// The quoting behavior of Excel appears to be to be dynamic.
+/// Strings are quoted if the contain the quote character or the separator.
+/// Quoting is piecemeal on individual cell values it is not "controlled" by the column.
+///
+/// An example:
+///
+/// Id,Value,Comment
+/// 1,Hello world,"No comma, no quotes"
+/// 2,"Hello, world","Comma, no quotes"
+/// 3,"""Hello, world""",Comma and quotes
+/// 4,"""Hello word""","Quotes, no comma"
+
+
+let quotedValue (quote:char) (input:string) : string = 
     match input with
     | null -> sprintf "%c%c" quote quote
     | _ -> 
@@ -15,9 +28,15 @@ let quoteValue (quote:char) (input:string) : string =
         let text = input.Replace(quoteStr, quoteStr+quoteStr) 
         sprintf "%c%s%c" quote text quote
 
+let quoteIfNecessary (quote:char) (separator:char) (input:string) : string = 
+    if input.Contains(quote.ToString()) || input.Contains(separator.ToString()) || input.Contains("\n") then
+        quotedValue quote input
+    else input
 
-/// Cell must allow reify whether a value is quoted.
-/// Then the printer can decide what charater to use for quoting.
+
+
+/// A cell can be always quoted (Quoted) or quoted if it contains
+/// the quote character or the separator.
 type Cell = 
     | Cell of String
     | Quoted of String
@@ -26,10 +45,10 @@ type Cell =
         | Cell(s) -> s
         | Quoted(s) -> sprintf "'%s'" s
 
-    member x.Output (quoteChar:char) = 
+    member internal x.Output (quoteChar:char) (separator:char) : string = 
         match x with
-        | Cell s -> s
-        | Quoted(s) -> quoteValue quoteChar s
+        | Cell s -> quoteIfNecessary quoteChar separator s
+        | Quoted s -> quotedValue quoteChar s
     
 
 
@@ -37,22 +56,34 @@ type OutputOptions =
     { Separator: char     
       Quote: char }
 
+let defaultOutputOptions : OutputOptions = 
+    { Separator = ','; Quote = '"' }
+
 [<Struct>]
 type Row = 
-    | Row of Cell list
-    member internal x.Cells = match x with | Row(xs) -> xs
+    val private cells : Cell list
+
+    new (cells: Cell list) = { cells = cells }
+
+    member x.Cells
+        with get() = x.cells
+
+    member internal x.StreamOutput (sw:StreamWriter) (quoteChar:char) (separator:char) : unit = 
+        let rec work (cells:Cell list) = 
+            match cells with
+            | [] -> sw.Write("\n")
+            | [x] -> 
+                sw.Write (x.Output quoteChar separator); sw.Write("\n")
+            | x :: xs -> 
+                sw.Write (x.Output quoteChar separator); sw.Write(separator); work xs
+        work x.Cells
+
+
 /// Prints 'true' or 'false' (unquoted).
 let csvBool (value:bool) : Cell = Cell <| value.ToString()
 
-let private outputRow (sw:StreamWriter) (opts:OutputOptions) (row:Row) : unit = 
-    let rec work (cells:Cell list) = 
-        match cells with
-        | [] -> sw.Write("\n")
-        | [x] -> 
-            sw.Write (x.Output (opts.Quote)); sw.Write("\n")
-        | x :: xs -> 
-            sw.Write (x.Output (opts.Quote)); sw.Write(opts.Separator); work xs
-    work row.Cells
+let csvString (value:string) : Cell = Cell <| value
+
 
 /// FSharp.Data API:
 /// table.Save(writer = sw, separator = ',', quote = '"' )
@@ -60,8 +91,39 @@ let private outputRow (sw:StreamWriter) (opts:OutputOptions) (row:Row) : unit =
 
 /// So favor an object oriented API.
 
-let outputCsv (sw:StreamWriter) (opts:OutputOptions) (rows:seq<Row>) : unit = 
-    Seq.iter (outputRow sw opts) rows
+let defaultQuote : Char = '"'
+let defaultSeparator : Char = ','
+
+type Csv = 
+    val private headers : option<string list>
+    val mutable private separator : char
+    val mutable private quoteChar : char
+    val private rows : seq<Row>
+
+    new (headers: string list, rows : seq<Row>) = 
+        { headers = Some <| headers 
+        ; separator = defaultSeparator
+        ; quoteChar = defaultQuote
+        ; rows = rows }
+    
+    member x.Separator
+        with get() = x.separator
+        and  set(v:char) = x.separator <- v
+
+    member x.QuoteChar
+        with get() = x.quoteChar
+        and  set(v:char) = x.quoteChar <- v
+
+    member x.Save (sw:StreamWriter) : unit = 
+        match x.headers with
+        | Some xs -> 
+            let row = new Row(List.map csvString xs)
+            row.StreamOutput sw x.quoteChar x.separator  
+        | None -> ()
+            
+    
+//let outputCsv (sw:StreamWriter) (opts:OutputOptions) (rows:seq<Row>) : unit = 
+//    Seq.iter (outputRow sw opts) rows
 
 
 
--- test/Test02.fsx
+++ test/Test02.fsx
@@ -8,12 +8,16 @@ open FSharp.Data
 #load "..\src\DynaCsv\Common.fs"
 #load "..\src\DynaCsv\CsvOutput2.fs"
 open DynaCsv.Common
-
+open DynaCsv.CsvOutput2
 
 let test01 () = 
     let inputPath = Path.Combine( __SOURCE_DIRECTORY__ , "..", @"data\hospitals.csv")
     let csv = providerReadCsv true "\t" '"' inputPath
     printfn "%s" <| csv.ToString()
     printfn "%A" <| csv.Headers
+    printfn "%A" << Seq.take 3 <| csv.Rows
 
-
+let test02 () = 
+    let csv = new Csv(headers = ["One";"Two"], rows = List.toSeq [] )
+    csv.Separator <- '\t'
+    printfn "%A" <| csv.Separator
